8ff0d3bd4a8f93383ab57d0f1cc935775c44bfc1
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-06-06 09:23:09 +0530

    Add files via upload

---
+++ Calci.java
@@ -0,0 +1,10 @@
+package Calci;
+import java.util.Scanner;
+public class main{
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		int n;
+		n = s.nextInt();
+		System.out.println("The input you provided is " + n);
+}
+}
